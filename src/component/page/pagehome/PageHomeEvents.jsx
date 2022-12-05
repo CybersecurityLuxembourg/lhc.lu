9e103ae75f7f6c616d3d3f87718db9a693594307
Alexis <alexis.prunier@securitymadein.lu>
2022-12-05 11:32:23 +0100

    Improve home page

--- src/component/page/pagehome/PageHomeEvents.jsx
+++ src/component/page/pagehome/PageHomeEvents.jsx
@@ -89,54 +89,60 @@ export default class PageHomeEvents extends React.Component {
 	}
 
 	render() {
-		if (!this.state.events) {
-			return <div className={"col-md-12"}>
-				<Loading
-					height={300}
-				/>
-			</div>;
-		}
-
-		if (this.state.events.length === 0) {
-			return <div className={"col-md-12"}>
-				<Message
-					text={"No event found"}
-					height={300}
-				/>
-			</div>;
-		}
-
 		return <div id="PageHomeEvents" className="PageHome-section">
 			<div className="page max-sized-page">
-				<div className="row row-spaced">
-					<div className={"col-md-8"}>
-						<h2>Upcoming events</h2>
-
-						<div className={"row"}>
-							{this.state.events.map((c, i) => <div
-								key={c.id}
-								className={"col-md-12"}>
-								<SmallArticle
-									info={c}
-								/>
-							</div>)}
+				{!this.state.events
+					&& <div className="row row-spaced">
+						<div className={"col-md-12"}>
+							<Loading
+								height={300}
+							/>
 						</div>
 					</div>
+				}
 
-					<div className={"col-md-1"}/>
-
-					<div className={"col-md-3"}>
-						<div className={"PageHomeEvents-image"}>
-							{this.state.events[this.state.selectedEvent].image
-								? <img
-									src={getApiURL() + "public/get_public_image/"
-										+ this.state.events[this.state.selectedEvent].image}
-									alt="Article image"/>
-								: <NoImage/>
-							}
+				{this.state.events && this.state.events.length === 0
+					&& <div className="row row-spaced">
+						<div className={"col-md-12"}>
+							<Message
+								content={"No event found"}
+								height={300}
+							/>
 						</div>
 					</div>
-				</div>
+				}
+
+				{this.state.events && this.state.events.length > 0
+					&& <div className="row row-spaced">
+						<div className={"col-md-8"}>
+							<h2>Upcoming events</h2>
+
+							<div className={"row"}>
+								{this.state.events.map((c, i) => <div
+									key={c.id}
+									className={"col-md-12"}>
+									<SmallArticle
+										info={c}
+									/>
+								</div>)}
+							</div>
+						</div>
+
+						<div className={"col-md-1"}/>
+
+						<div className={"col-md-3"}>
+							<div className={"PageHomeEvents-image"}>
+								{this.state.events[this.state.selectedEvent].image
+									? <img
+										src={getApiURL() + "public/get_public_image/"
+											+ this.state.events[this.state.selectedEvent].image}
+										alt="Article image"/>
+									: <NoImage/>
+								}
+							</div>
+						</div>
+					</div>
+				}
 			</div>
 		</div>;
 	}
--- src/component/page/pagehome/PageHomeNews.jsx
+++ src/component/page/pagehome/PageHomeNews.jsx
@@ -73,23 +73,6 @@ export default class PageHomeNews extends React.Component {
 	}
 
 	render() {
-		if (!this.state.news) {
-			return <div className={"col-md-12"}>
-				<Loading
-					height={300}
-				/>
-			</div>;
-		}
-
-		if (this.state.news.length === 0) {
-			return <div className={"col-md-12"}>
-				<Message
-					content={"No news found"}
-					height={300}
-				/>
-			</div>;
-		}
-
 		return <div id="PageHomeNews" className="PageHome-section">
 			<div className="page max-sized-page">
 				<div className="row row-spaced">
@@ -98,45 +81,68 @@ export default class PageHomeNews extends React.Component {
 					</div>
 				</div>
 
-				<div className="row row-spaced">
-					<div className={"col-md-3"}>
-						<div className={"PageHomeNews-image"}>
-							{this.state.news[this.state.selectedNews].image
-								? <img
-									src={getApiURL() + "public/get_public_image/"
-										+ this.state.news[this.state.selectedNews].image}
-									alt="Article image"/>
-								: <NoImage/>
-							}
+				{!this.state.news
+					&& <div className="row row-spaced">
+						<div className={"col-md-12"}>
+							<Loading
+								height={300}
+							/>
 						</div>
 					</div>
+				}
 
-					<div className={"col-md-1"}/>
+				{this.state.news && this.state.news.length === 0
+					&& <div className="row row-spaced">
+						<div className={"col-md-12"}>
+							<Message
+								content={"No news found"}
+								height={300}
+							/>
+						</div>
+					</div>
+				}
 
-					<div className={"col-md-8"}>
-						<h2>Latest news</h2>
-
-						<div className={"row"}>
-							{this.state.news.map((c, i) => <div
-								key={c.id}
-								className={"col-md-12"}>
-								<SmallArticle
-									info={c}
-								/>
-							</div>)}
+				{this.state.news && this.state.news.length > 0
+					&& <div className="row row-spaced">
+						<div className={"col-md-3"}>
+							<div className={"PageHomeNews-image"}>
+								{this.state.news[this.state.selectedNews].image
+									? <img
+										src={getApiURL() + "public/get_public_image/"
+											+ this.state.news[this.state.selectedNews].image}
+										alt="Article image"/>
+									: <NoImage/>
+								}
+							</div>
 						</div>
 
-						<div className={"row"}>
-							<div className={"col-md-8"}>
-								<Link
-									to={"/news"}
-									className="Article-link">
-									<b>See all</b>
-								</Link>
+						<div className={"col-md-1"}/>
+
+						<div className={"col-md-8"}>
+							<h2>Latest news</h2>
+
+							<div className={"row"}>
+								{this.state.news.map((c, i) => <div
+									key={c.id}
+									className={"col-md-12"}>
+									<SmallArticle
+										info={c}
+									/>
+								</div>)}
+							</div>
+
+							<div className={"row"}>
+								<div className={"col-md-8"}>
+									<Link
+										to={"/news"}
+										className="Article-link">
+										<b>See all</b>
+									</Link>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
+				}
 			</div>
 		</div>;
 	}
