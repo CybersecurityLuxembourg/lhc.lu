e234cf9b9c8edd52aa60a0acbe5be76982412d3a
Alexis <alexis.prunier@securitymadein.lu>
2022-12-22 15:41:36 +0100

    Add partners in about

--- src/component/page/pagehome/PageHomeAboutLHC.jsx
+++ src/component/page/pagehome/PageHomeAboutLHC.jsx
@@ -1,13 +1,144 @@
 import React from "react";
 import "./PageHomeAboutLHC.css";
 import { getApiURL } from "../../../utils/env.jsx";
+import Graph from "react-graph-vis";
 
 export default class PageHomeAboutLHC extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			data: {
+				nodes: [
+					{
+						id: -1,
+						title: "",
+						color: { border: "white", background: "#193c6d" },
+						font: { color: "white", size: 14 },
+						shape: "image",
+						image: "/img/lhc-logo-full.png",
+						chosen: false,
+					},
+					this.getTextNode(10, "Economy"),
+					this.getTextNode(11, "Citizens"),
+					this.getTextNode(12, "Industry"),
+					this.getTextNode(13, "Education"),
+					this.getTextNode(14, "Municipalities"),
+					this.getTextNode(15, "Health"),
+					this.getTextNode(16, "Defence"),
+					this.getTextNode(17, "Research"),
+					this.getTextNode(18, "and more..."),
+
+					this.getImageNode(100, "/img/lhc-logo-full.png", "https://www.google.com"),
+					this.getImageNode(101, "/img/lhc-logo-full.png", "https://www.google.com"),
+					this.getImageNode(102, "/img/lhc-logo-full.png", "https://www.google.com"),
+					this.getImageNode(103, "/img/lhc-logo-full.png", "https://www.google.com"),
+					this.getImageNode(104, "/img/lhc-logo-full.png", "https://www.google.com"),
+					this.getImageNode(105, "/img/lhc-logo-full.png", "https://www.google.com"),
+					this.getImageNode(106, "/img/lhc-logo-full.png", "https://www.google.com"),
+					this.getImageNode(107, "/img/lhc-logo-full.png", "https://www.google.com"),
+					this.getImageNode(108, "/img/lhc-logo-full.png", "https://www.google.com"),
+				],
+				edges: [
+					this.getEdge(-1, 10, true),
+					this.getEdge(-1, 11, true),
+					this.getEdge(-1, 12, true),
+					this.getEdge(-1, 13, true),
+					this.getEdge(-1, 14, true),
+					this.getEdge(-1, 15, true),
+					this.getEdge(-1, 16, true),
+					this.getEdge(-1, 17, true),
+					this.getEdge(-1, 18, true),
+
+					this.getEdge(10, 100),
+					this.getEdge(10, 101),
+					this.getEdge(11, 102),
+
+					this.getEdge(12, 103),
+					this.getEdge(13, 104),
+					this.getEdge(14, 105),
+					this.getEdge(15, 106),
+					this.getEdge(16, 107),
+					this.getEdge(17, 108),
+				],
+			}
+		}
+	}
+
+	getTextNode(id, title) {
+		return {
+			id,
+			title,
+			label: title,
+			color: { border: "white", background: "#193c6d" },
+			font: { color: "white", size: 14 },
+			shape: "box",
+			chosen: false,
+		};
+	}
+
+	getImageNode(id, img, link) {
+		return {
+			id,
+			title: "",
+			shape: "image",
+			image: "/img/lhc-logo-full.png",
+			chosen: false,
+			link,
+		};
+	}
+
+	getEdge(id1, id2, transparent=false) {
+		return {
+			from: id1,
+			to: id2,
+			color: {
+				color: transparent ? "transparent" : "white",
+			},
+			chosen: false,
+			width: 2,
+			smooth: {
+				enabled: true,
+				type: "curvedCCW",
+				roundness: .1,
+			},
+		};
 	}
 
 	render() {
+		const options = {
+			physics: { barnesHut: { springLength: 100, springConstant: .2 } },
+			layout: {
+			},
+			nodes: {
+				margin: {
+					top: 5,
+					bottom: 5,
+					left: 20,
+					right: 20,
+				},
+				font: {
+					multi: "html",
+				},
+			},
+			edges: {
+				color: "#000000",
+			},
+			height: "500px",
+		};
+
+		const events = {
+			select: ({ nodes, edges }) => {
+				const selectedNode = this.state.data.nodes
+					.filter((n) => n.id === nodes[0])
+					.pop();
+
+				if (selectedNode && selectedNode.link) {
+					window.open(selectedNode.link);
+				}
+			},
+	    }
+
 		return <div id="PageHomeAboutLHC" className="PageHome-section">
 			<div className="page max-sized-page">
 				<div className="row row-spaced">
@@ -24,7 +155,7 @@ export default class PageHomeAboutLHC extends React.Component {
 
 					<div className="col-md-1"/>
 
-					<div className="col-md-6">
+					<div className="col-md-6 row-spaced">
 						<h2>Make Luxembourg a pioneer in the open cybersecurity data economy</h2>
 
 						<p>Luxembourg House of Cybersecurity is the backbone of leading-edge cyber
@@ -35,6 +166,14 @@ export default class PageHomeAboutLHC extends React.Component {
 						together with its two hosted centres CIRCL (Computer Incident Response
 						Center Luxembourg) and NC3 (National Cybersecurity Competence Center) as
 						well as its following partners, support, foster and serve:</p>
+					</div>
+
+					<div className="col-md-12">
+						<Graph
+							graph={this.state.data}
+							options={options}
+							events={events}
+						/>
 					</div>
 				</div>
 			</div>
