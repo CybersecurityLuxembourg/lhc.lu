8fe6b554c3abf0d18df6709d25c59c97af2aa09f
Nicky Mezzina <nicky.mezzina@nc3.lu>
2026-03-26 14:42:51 +0100

    Fix flex layout for narrow viewports

--- src/component/bar/Footer.jsx
+++ src/component/bar/Footer.jsx
@@ -16,7 +16,7 @@ export default class Footer extends React.Component {
 				<div className="Footer-content">
 					<div className="row">
 						<div className="col-md-2 Footer-contact">
-							<div className="row">
+							<div>
 								<img
 									className={"Footer-logo"}
 									src="/img/lhc-logo-white.png"
@@ -29,13 +29,13 @@ export default class Footer extends React.Component {
 								<div>(+352) 274 00 98 601</div>
 							</div>
 
-							<div className="row">
+							<div>
 								<div className="Footer-link">
 									<a href="mailto:info@lhc.lu">info@lhc.lu</a>
 								</div>
 							</div>
 
-							<div className="row">
+							<div>
 								<div className="Footer-link">
 									<a
 										href="https://circl.lu/report/"
@@ -110,45 +110,37 @@ export default class Footer extends React.Component {
 							</div>
 							<div className="row">
 								<div className="col-md-12">
-									<div className="row Footer-center-logos">
-										<div className="col-md-3">
-											<div className="Footer-link">
-												<a href="https://www.nc3.lu" target="_blank" rel="noopener noreferrer">
-													<img
-														src="/img/nc3-lu-horizontal-negative.svg"
-														alt="NC3 logo"
-														height="64"
-													/>
-												</a>
-											</div>
+									<div className="Footer-center-logos">
+										<div className="Footer-link">
+											<a href="https://www.nc3.lu" target="_blank" rel="noopener noreferrer">
+												<img
+													src="/img/nc3-lu-horizontal-negative.svg"
+													alt="NC3 logo"
+													height="64"
+												/>
+											</a>
 										</div>
 
-										<div className="col-md-3">
-											<div className="Footer-link">
-												<a href="https://www.circl.lu" target="_blank"
-												   rel="noopener noreferrer">
-													<img
-														src="/img/circl-lu-logo-white.svg"
-														alt="CIRCL logo"
-													 	height="64"
-													/>
-												</a>
-											</div>
-										</div>
-										<div className="col-md-3">
-											<div className="Footer-link">
+										<div className="Footer-link">
+											<a href="https://www.circl.lu" target="_blank"
+											   rel="noopener noreferrer">
 												<img
-													src="/img/luxembourg-cybersecurity-factory-logo-white-rgb.svg"
-													alt="LUXEMBOURG CYBERSECURITY FACTORY logo" height="64"
+													src="/img/circl-lu-logo-white.svg"
+													alt="CIRCL logo"
+													height="64"
 												/>
-											</div>
+											</a>
 										</div>
-										<div className="col-md-3">
-											<div className="Footer-link">
-												<Link to="/lucya">
-													<img src="/img/lucya-logo-white.svg" alt="LUCYA logo" height="64"/>
-												</Link>
-											</div>
+										<div className="Footer-link">
+											<img
+												src="/img/luxembourg-cybersecurity-factory-logo-white-rgb.svg"
+												alt="LUXEMBOURG CYBERSECURITY FACTORY logo" height="64"
+											/>
+										</div>
+										<div className="Footer-link">
+											<Link to="/lucya">
+												<img src="/img/lucya-logo-white.svg" alt="LUCYA logo" height="64"/>
+											</Link>
 										</div>
 									</div>
 									<div className="row">
@@ -163,7 +155,7 @@ export default class Footer extends React.Component {
 						<div className="col-md-4"/>
 						<div className="col-md-3">
 
-							<div className="Footer-link">
+							<div className="Footer-link Footer-legal-links">
 								<a
 									href={"https://api.cybersecurity.lu/public/get_public_document/LHC_Legal_Notice.pdf"}
 									rel="noreferrer noopener"
